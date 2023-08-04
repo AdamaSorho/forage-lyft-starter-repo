58039848a0e6fa9c5e3c6d4bbce8fe5fc009a1d9
AdamaSorho <djomantcha@gmail.com>
2023-08-04 01:08:28 -0500

    Added unit tests

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -7,6 +7,6 @@ class NubbinBattery(Battery):
         self.current_date = current_date
 
     def needs_service(self) -> bool:
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date + 4)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
 
         return service_threshold_date < self.current_date
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -7,6 +7,6 @@ class SpindlerBattery(Battery):
         self.current_date = current_date
 
     def needs_service(self) -> bool:
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date + 2)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
 
         return service_threshold_date < self.current_date
