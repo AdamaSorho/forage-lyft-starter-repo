12fa5b9f5e46cf3ec552e297dbfffc9d407f9968
AdamaSorho <djomantcha@gmail.com>
2023-08-03 23:18:27 -0500

    refactored the codebase to reflect the new design

---
+++ car_factory.py
@@ -0,0 +1,43 @@
+from model.car import Car
+from engine.capulet_engine import CapuletEngine
+from engine.sternman_engine import SternmanEngine
+from engine.willoughby_engine import WilloughbyEngine
+from battery.nubbin_battery import NubbinBattery
+from battery.spindler_battery import SpindlerBattery
+
+class CarFactory:
+    
+    @staticmethod
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = SpindlerBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_palindrome(current_date, last_service_date, warning_light_on) -> Car:
+        engine = SternmanEngine(warning_light_is_on=warning_light_on)
+        battery = SpindlerBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = WilloughbyEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
+    
+    @staticmethod
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage) -> Car:
+        engine = CapuletEngine(current_mileage, last_service_mileage)
+        battery = NubbinBattery(last_service_date, current_date)
+
+        return Car(engine, battery)
---
+++ model/car.py
@@ -0,0 +1,11 @@
+from service.serviceable import Serviceable
+
+class Car(Serviceable):
+    
+    def __init__(self, engine, battery) -> None:
+        super().__init__()
+        self.engine = engine
+        self.battery = battery
+
+    def needs_service(self) -> bool:
+        return self.engine.needs_service() or self.battery.needs_service()
